32d1d5d47529ec30d2f74e5ff35b9354144898a7
austenFs <aust.furu@gmail.com>
2022-03-23 18:09:15 -0700

    added button

--- app/src/main/java/edu/up/cs301/battleship/BattleShipHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/battleship/BattleShipHumanPlayer.java
@@ -1,6 +1,8 @@
 package edu.up.cs301.battleship;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 import edu.up.cs301.game.GameFramework.GameMainActivity;
 import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
@@ -30,5 +32,15 @@ public class BattleShipHumanPlayer extends GameHumanPlayer {
     public void setAsGui(GameMainActivity activity) {
         this.myActivity = activity;
         activity.setContentView(R.layout.setup_phase);
+        Button nextButton = activity.findViewById(R.id.confirm_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.setContentView(R.layout.midgame);
+
+            }
+        });
+
+
     }
 }
