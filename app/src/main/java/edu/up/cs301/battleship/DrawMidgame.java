b07b55bf51f9e122b33bfbc6790a013b3d6a1e17
austenFs <aust.furu@gmail.com>
2022-03-25 17:21:51 -0700

    Beta for touch listener/drawing

--- app/src/main/java/edu/up/cs301/battleship/DrawMidgame.java
+++ app/src/main/java/edu/up/cs301/battleship/DrawMidgame.java
@@ -48,8 +48,8 @@ public class DrawMidgame extends SurfaceView{
     @Override
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
-        TouchListener listener = new TouchListener(this, this.context);
-        this.setOnTouchListener(listener);
+//        TouchListener listener = new TouchListener(this, this.context);
+//        this.setOnTouchListener(listener);
 
         //FLOAT - like a double but half the number of bits
         //drawOval(x,y x2, y2, paint);
