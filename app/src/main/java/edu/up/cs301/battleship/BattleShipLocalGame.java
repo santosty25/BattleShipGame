1e8f55fa0156224ad79eefbcdcaac052e97e2895
7angsan <leeste25@up.edu>
2022-04-13 17:41:11 -0700

    checkIfGameOver() fixed*.

--- app/src/main/java/edu/up/cs301/battleship/BattleShipLocalGame.java
+++ app/src/main/java/edu/up/cs301/battleship/BattleShipLocalGame.java
@@ -19,7 +19,6 @@ import edu.up.cs301.game.GameFramework.players.GamePlayer;
     public class BattleShipLocalGame extends LocalGame {
 
         private BattleShipGameState localState;
-        private BattleShipMainActivity main;
         private boolean player0Ready = false;
         private boolean player1Ready = false;
 
@@ -77,11 +76,11 @@ import edu.up.cs301.game.GameFramework.players.GamePlayer;
             int winner = gameState.checkPlayerFleet();
             if (winner == 0) {
                 gameState.setPhase(BattleShipGameState.END_PHASE);
-                return "Player 0 has won. ";
+                return BattleShipMainActivity.player0Name + " has won. ";
             }
             else if (winner == 1) {
                 gameState.setPhase(BattleShipGameState.END_PHASE);
-                return "Player 1 has won. ";
+                return BattleShipMainActivity.player1Name + " has won. ";
             }
             return null;
         }
