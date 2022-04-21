fa892f8042db11a3ea13e759b6b96e8454b0570a
7angsan <leeste25@up.edu>
2022-04-21 16:56:09 -0700

    Tests actually fixed

--- app/src/test/java/edu/up/cs301/battleship/BattleShipGameStateTest.java
+++ app/src/test/java/edu/up/cs301/battleship/BattleShipGameStateTest.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 
 public class BattleShipGameStateTest {
 
+
     @Test
-    public void setHasShip() {
+    public void setHasShip() throws Exception {
         BattleShipGameState bgs = new BattleShipGameState();
         bgs.setHasShip(2, 10, false, 0);
         boolean hasShip = bgs.getPlayersBoard()[0].getHasShip(2, 10);
@@ -15,24 +16,45 @@ public class BattleShipGameStateTest {
     }
 
     @Test
-    public void canFire() {
+    public void canFire() throws Exception {
+        BattleShipGameState gameState = new BattleShipGameState();
+        Coordinates coord = new Coordinates(false, true, 4, 5);
 
+        boolean fire = gameState.canFire(coord);
+        assertTrue(fire);
     }
 
     @Test
-    public void placeShip() {
+    public void placeShip() throws Exception{
     }
 
     @Test
-    public void printFire() {
+    public void printFire() throws Exception{
+        BattleShipGameState gameState = new BattleShipGameState();
+        Coordinates coord = new Coordinates(false, false, 4, 5);
+        assertNotNull(gameState.printFire(4,5,true));
+
+        // Tests to see what happens if canfire is false and someone clicks it
+        Coordinates coordTwo = new Coordinates(false, false, 4, 5);
+        assertNotNull(gameState.printFire(100,100,false));
     }
 
     @Test
-    public void printPlaceShip() {
+    public void printPlaceShip() throws Exception {
+        BattleShipGameState gameState = new BattleShipGameState();
+        Coordinates shipOne = new Coordinates(false, false, 50,50);
+        Coordinates shipTwo = new Coordinates(false, false, 50,50);
+        Coordinates[] ships = {shipOne, shipTwo};
+        assertNotNull(gameState.printPlaceShip(ships, false));
+
+        // Tests to see what happens if canfire is false and someone clicks it
+        assertNotNull(gameState.printFire(100,100,false));
     }
 
     @Test
-    public void setPlayersFleet() {
+    public void setPlayersFleet() throws Exception {
+        BattleShipGameState gameState = new BattleShipGameState();
+        gameState.setPlayersFleet(gameState.getPlayersFleet(), 1 );
     }
 
     @Test
